959c47f0a388eb28b9eb19a7ee43680d59034206
reemsh2000 <sh951652@gmail.com>
2021-08-10 15:42:36 +0300

    html file and css

---
+++ public/js/ManipulateDom.js
@@ -0,0 +1,6 @@
+const cardsContainer=document.querySelector('.cards-sontainer')
+const day=document.querySelector('#day')
+const month=document.querySelector('#month')
+const createCard=(tag,parent)=>{
+const item=document.createElement
+}
