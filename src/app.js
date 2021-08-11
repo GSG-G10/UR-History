57e2e75a93d525b9770813863f589161873ff32f
reemsh2000 <sh951652@gmail.com>
2021-08-11 16:49:14 +0300

    edit port

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.set('port', process.env.port || 5000);
+app.set('port', process.env.PORT || 5000);
 app.use(compression());
 app.use(express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' }));
 app.use(controllers);
